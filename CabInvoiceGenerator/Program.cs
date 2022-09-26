376f4fd09300a80d9b28f62566b22b4f9be0f353
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-26 16:41:56 +0530

    Cab Invoice Generator Problem

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Problem");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+}
---
+++ CabInvoiceGenerator/RideType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+
+    /// <summary>
+    /// Enum class for Cab Invoice
+    /// </summary>
+    public enum RideType
+    {
+        NORMAL,
+        PREMIUM
+    }
+}
